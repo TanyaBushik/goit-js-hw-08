0639f5b62ba8f1d37894bed289ecd7f9c9ffbb7c
TanyaBushik <tanya.bushik@gmail.com>
2023-03-30 22:49:24 +0300

    code for task  added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,47 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+
+const imageItem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join('');
+
+// galleryListEl.insertAdjacentHTML("beforeend", imageItem);
+
+galleryListEl.innerHTML = imageItem;
+
+galleryListEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryListEl.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
