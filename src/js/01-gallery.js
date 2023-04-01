c8c8fe733496d787a7d23dc2625f1a2784d50acc
TanyaBushik <tanya.bushik@gmail.com>
2023-04-02 01:14:38 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,49 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
 
 const galleryListEl = document.querySelector('.gallery');
 
-const imageItem = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </li>`
-  )
-  .join('');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// galleryListEl.insertAdjacentHTML("beforeend", imageItem);
 
-galleryListEl.innerHTML = imageItem;
-
-galleryListEl.addEventListener('click', onImageClick);
-
-function onImageClick(event) {
-  event.preventDefault();
-
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-
-  instance.show();
-
-  galleryListEl.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  });
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return ` <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}"
+       alt="${description}" />
+    </a>`
+  }).join('');
 }
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
