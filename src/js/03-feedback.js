e7649ca7dc0042447993570cdc83e211df77c966
TanyaBushik <tanya.bushik@gmail.com>
2023-04-03 01:09:23 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onSubmitClick);
+form.addEventListener('input', throttle(onInputData, 500));
+
+
+checkStorageContent();
+
+const formData = {};
+
+function onInputData(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    };
+
+
+function onSubmitClick(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function checkStorageContent() {
+    const savedFeedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+    if (savedFeedbackForm) {
+        email.value = savedFeedbackForm.email;
+        message.value = savedFeedbackForm.message;
+    }
+}
