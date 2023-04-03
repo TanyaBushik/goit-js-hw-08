0028267f09f5e437be314f46ef69cd43537be8af
TanyaBushik <tanya.bushik@gmail.com>
2023-04-03 23:18:37 +0300

    added check for form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,20 +13,27 @@ const formData = {};
 
 function onInputData(evt) {
     formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     };
 
 
 function onSubmitClick(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
+
+    const { email, message } = form.elements;
+    console.log({ email: email.value, message: message.value });
+
+     if (email.value === '' || message.value === '') {
+       return alert('Please fill in all the fields!');
+     }
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function checkStorageContent() {
     const savedFeedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
-const email = document.querySelector('.feedback-form input');
-const message = document.querySelector('.feedback-form textarea');
+// const email = document.querySelector('.feedback-form input');
+// const message = document.querySelector('.feedback-form textarea');
     if (savedFeedbackForm) {
         email.value = savedFeedbackForm.email;
         message.value = savedFeedbackForm.message;
